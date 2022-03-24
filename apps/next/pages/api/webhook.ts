1d99c00ad28369050f8a502ce639ef6afd447cd4
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-03-23 21:50:32 -0500

    Test

--- apps/next/pages/api/webhook.ts
+++ apps/next/pages/api/webhook.ts
@@ -1,4 +1,10 @@
 export default function handler(req, res) {
-  console.log(req.body);
-  res.status(200).json({ name: 'get webhooks n stuff test' });
+  if (req.method === 'POST') {
+    console.log(req.body);
+    return res.status(200).json({ name: 'ok' });
+  }
+
+  if (req.method === 'GET') {
+    res.status(200).json({ name: 'get webhooks n stuff test' });
+  }
 }
