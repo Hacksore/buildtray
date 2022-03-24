9686e386a2db062ebe657e3216fe376c4a21190c
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-03-24 09:42:14 -0500

    Test CD

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,51 +1,50 @@
-let count = 0
+let count = 0;
 
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
 
 async function handleSession(websocket) {
-  websocket.accept()
+  websocket.accept();
   websocket.addEventListener("message", async ({ data }) => {
     websocket.send(JSON.stringify({ hello: "world" }));
-
-  })
+  });
 
   websocket.addEventListener("close", async evt => {
     // Handle when a client closes the WebSocket connection
-    console.log(evt)
-  })
+    console.log(evt);
+  });
 }
 
 const websocketHandler = async request => {
-  const upgradeHeader = request.headers.get("Upgrade")
+  const upgradeHeader = request.headers.get("Upgrade");
   if (upgradeHeader !== "websocket") {
-    return new Response("Expected websocket", { status: 400 })
+    return new Response("Expected websocket", { status: 400 });
   }
 
-  const [client, server] = Object.values(new WebSocketPair())
-  await handleSession(server)
+  const [client, server] = Object.values(new WebSocketPair());
+  await handleSession(server);
 
   return new Response(null, {
     status: 101,
-    webSocket: client
-  })
-}
+    webSocket: client,
+  });
+};
 
 async function handleRequest(request) {
   try {
-    const url = new URL(request.url)
+    const url = new URL(request.url);
     switch (url.pathname) {
-      case '/':
-        return new Response("build tray api", { status: 200 })
-      case '/build':
-        return new Response("TODO", { status: 418 })
-      case '/ws':
-        return websocketHandler(request)
+      case "/":
+        return new Response("build tray api", { status: 200 });
+      case "/build":
+        return new Response("TODO", { status: 418 });
+      case "/ws":
+        return websocketHandler(request);
       default:
-        return new Response("Not found", { status: 404 })
+        return new Response("Not found", { status: 404 });
     }
   } catch (err) {
-    return new Response(err.toString())
+    return new Response(err.toString());
   }
 }
--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    target: "webworker",
-    entry: "./index.js",
-    mode: "production",
-    devtool: "source-map",
+  target: "webworker",
+  entry: "./index.js",
+  mode: "production",
+  devtool: "source-map",
 };
